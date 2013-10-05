bee2a0f0f19d77bc206a49643209d53c3926f434
Michael Williams <draringi@draringi.net>
2013-10-05 13:37:04 -0400

    added base class structure

--- AniMcGuild/models.py
+++ AniMcGuild/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class HeroClass(models.Model):
+    name = models.CharField(max_length=30)
+    hp_bonus = models.IntegerField()
+    ac_bonus = models.IntegerField()
+    ref_bonus = models.IntegerField()
+    
+
+class Skills(models.Model):
+    ATTACK_TYPES = (
+        ('ME', "Melee"),
+        ('RN', "Ranged"),
+    )
+    name = models.CharField(max_length=30)
+    attack_type = models.CharField(max_length=2, choices=ATTACK_TYPES)
+    hit = models.CharField(max_length=30)
+    silences = models.BooleanField(default=False)
+
+class Hero(models.Model):
+    hero_class = models.ForeignKey(HeroClass)
